c9129e44a744a439e0cdceb9e58dcc2acab66ad5
AnnaHeisig <an.heisig@duocuc.cl>
2024-05-11 14:29:36 -0400

    conversión precio

--- jquery.js
+++ jquery.js
@@ -9,14 +9,22 @@ $(function(){
 
     $('.btnConversion').click(function(){
         $.getJSON('https://mindicador.cl/api', function(data) {
-           var dolar = data.dolar.valor;
-           var precio = $('.precioJuego').html(); 
-           var precioDolarizado = Math.round(precio / dolar);
-            $('.precioJuego').text("$"+ precioDolarizado + " USD" ); 
+            if ($('.btnConversion').text().trim() == "convertir a doláres."){
+                var dolar = data.dolar.valor;
+                var precio = $('.precioJuego').text();
+                var precioDolarizado = Math.round(precio / dolar);
+                $('.precioJuegoConvertido').text("$"+ precioDolarizado + " USD");
+                $('.precioJuego').hide();
+                $('.precioJuegoConvertido').show();
+                $('.btnConversion').text("convertir a pesos."); 
+            }
+            else if ($('.btnConversion').text().trim() == "convertir a pesos."){
+                $('.precioJuego').show();
+                $('.precioJuegoConvertido').hide();
+                $('.btnConversion').text("convertir a doláres."); 
+            }
         }).fail(function() {
     console.log('Error al consumir la API!');
         })
     })
-
-
 })
