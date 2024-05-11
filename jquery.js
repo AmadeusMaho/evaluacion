b2634a139402b01c3e13eef9fb88ebe47bd56f9d
Mauri <maur.gatica@duocuc.cl>
2024-05-11 14:32:31 -0400

    pre-changes

--- jquery.js
+++ jquery.js
@@ -9,15 +9,23 @@ $(function(){
 
     $('.btnConversion').click(function(){
         $.getJSON('https://mindicador.cl/api', function(data) {
-            if ($('.btnConversion').text() == "Convertir a doláres."){
-                var dolar = data.dolar.valor;
-                var precio = $('.precioJuego').html(); 
-                var precioDolarizado = Math.round(precio / dolar);
-                 $('.precioJuego').text("$"+ precioDolarizado + " USD" ); 
-            }
-            else{
-                precioJuego.text(precio);
-            }
+
+        alert("precio: " + precio);
+        alert("dolar: " + dolar);
+        if($.trim($('.btnConversion').text=="convertir a doláres.")){
+            var dolar = data.dolar.valor;
+            const precioOriginal = $('.precioJuego').html(); 
+            var precioDolarizado = Math.round(precio / dolar);
+    
+             $('.precioJuego').text(precioDolarizado); 
+             $('.btnConversion').text("convertir a pesos.");   
+        }
+        else if($.trim($('.btnConversion').text=="convertir a pesos.")){
+            $('.precioJuego').text(precioOriginal); 
+            $('.btnConversion').text("convertir a doláres.");
+        }
+             
+
           
         }).fail(function() {
     console.log('Error al consumir la API!');
