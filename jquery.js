780fd23ebb89aa863429d8d496f4ab129385159a
Mauri <maur.gatica@duocuc.cl>
2024-05-11 12:41:06 -0400

    jquery dolar

--- jquery.js
+++ jquery.js
@@ -4,4 +4,20 @@ $(function(){
         alert("Función en desarrollo.");
     })
     
+
+    $('.btnConversion').click(function(){
+        $.getJSON('https://mindicador.cl/api', function(data) {
+           var dolar = data.dolar.valor;
+           var precio = $('.precioJuego').val(); //No agarra el valor del texto, queda en blanco. Apenas lo reciba y le podamos asignar el valor de precioDolarizado 
+                                                // debería funcionar (en teoría)
+           alert(precio);
+           alert("dolar actual: " + dolar);
+           var precioDolarizado = dolar * precio;
+            $('.precioJuego').val(precioDolarizado); 
+        }).fail(function() {
+    console.log('Error al consumir la API!');
+        })
     })
+
+
+})
